4e4c948d6eee5ebe7567aef5e14d8b65baef69df
Danielle Leblanc-Cyr <d.leblanc-cyr@creating-web.com>
2018-11-01 18:43:24 +0100

    Updates

---
+++ precache-manifest.477975f4cced73eec7ce9adac19b1e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "4eb2cd51ef5960f7eda6",
+    "url": "/markdown-previewer/static/js/main.4eb2cd51.chunk.js"
+  },
+  {
+    "revision": "e8b0ae4ca78e4779c8f8",
+    "url": "/markdown-previewer/static/js/1.e8b0ae4c.chunk.js"
+  },
+  {
+    "revision": "4eb2cd51ef5960f7eda6",
+    "url": "/markdown-previewer/static/css/main.299e1355.chunk.css"
+  },
+  {
+    "revision": "21dcbf3f5189cb4c1b5137c742958ee1",
+    "url": "/markdown-previewer/index.html"
+  }
+];
